bdc04903aacbc4aba6cf5a0e56c7d17f94d91013
mramirez092 <miguelramirez2210@gmail.com>
2023-04-04 18:00:05 +0000

    Proyecto terminado y renderizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const values = ["2", "3", "4", "5", "6", "7", "8", "9"];
+const suits = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function selectRandomCard() {
+  let valueIndex = Math.floor(Math.random() * values.length);
+  let suitIndex = Math.floor(Math.random() * suits.length);
+  let value = values[valueIndex];
+  let suit = suits[suitIndex];
+  return { value, suit };
+}
+
+onload = function hola() {
+  //let elementValue = document.querySelector(".number");
+  let elementSuit = document.querySelector(".span1");
+  let elementSuit2 = document.querySelector(".span2");
+  let card = selectRandomCard();
+  numberr.textContent = card.value;
+  elementSuit.textContent = card.suit;
+  elementSuit2.textContent = card.suit;
 };
